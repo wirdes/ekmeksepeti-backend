c35de500d2924d3c1f768bcf6bc2c8ca0b50250b
wirdes <45904810+wirdes@users.noreply.github.com>
2021-09-11 17:03:46 +0300

    feat:

--- routers/admin.js
+++ routers/admin.js
@@ -5,11 +5,13 @@ const {
   login,
   tokenVerify,
   addAddress,
+  updateProfile
 } = require("../controllers/admin");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/addAddress", addAddress);
 router.get("/tokenVerify/", tokenVerify);
+router.post("/updateProfile/", updateProfile);
 
 module.exports = router;
