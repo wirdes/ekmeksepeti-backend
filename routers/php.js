443fc04e09622c0f4ce85435587ffd7bd1a3fe4c
wirdes <45904810+wirdes@users.noreply.github.com>
2021-11-22 16:31:28 +0300

    feat:

--- routers/php.js
+++ routers/php.js
@@ -3,23 +3,55 @@ const router = express.Router();
 const axios = require("axios");
 const wallets = require("./wallet.json");
 const asyncHandler = require("express-async-handler");
+function isAlphaNumeric(str) {
+  var code, i, len;
 
-const controlWallet = (wallets, data, link) => {
-  let say = 0;
-
-  if (data.search("0x") > 0) {
-    say++;
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (
+      !(code > 47 && code < 58) && // numeric (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123)
+    ) {
+      // lower alpha (a-z)
+      return false;
+    }
   }
+  return true;
+}
+const controlWallet = (wallets, data, link) => {
+  let allwallets = [];
+
+  data1 = data.split(" ");
+  data1.forEach((wallet) => {
+    let abc = [];
+    const index = wallet.search("0x");
+
+    if (index > 0) {
+      for (let i = index; i < wallet.length; i++) {
+        if (isAlphaNumeric(wallet[i])) {
+          abc.push(wallet[i]);
+        } else {
+          break;
+        }
+      }
+    }
+    if (abc.length == 42) {
+      allwallets.push(abc.join(""));
+    }
+  });
 
   let winner = [];
   wallets.forEach((wallet) => {
-    if (data.search(wallet.wallet) > 0) {
+    if (allwallets.join("").search(wallet.wallet) > 0) {
+      a = data.search(wallet.wallet);
       winner.push(wallet.name);
     }
   });
-  if (say < 0) {
-    return ["site kontrol ediniz.", link, "hatalı", say];
+  if (allwallets.length === 0) {
+    return ["site kontrol ediniz", link];
   }
+
   if (winner.length > 0) {
     return winner;
   }
